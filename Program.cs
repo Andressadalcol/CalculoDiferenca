51863eb72f47d20d2a00ba9509fd111a021a5c97
Andressa Dal Col <104804693+Andressadalcol@users.noreply.github.com>
2023-12-24 12:24:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoDiferenca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c, d, df;
+            
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            Console.WriteLine( );
+
+            df= (a*b-c*d);
+            Console.WriteLine("df = "+ df);
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
