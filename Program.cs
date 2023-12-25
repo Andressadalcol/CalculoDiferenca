8208609ae639df900007036b3f8175f6b961cc72
Andressa Dal Col <104804693+Andressadalcol@users.noreply.github.com>
2023-12-25 18:31:12 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace CalculoDiferenca
         static void Main(string[] args)
         {
 
+            Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+            casas decimais conforme exemplos.
+            Fórmula da área: area = π . raio2
+            Considere o valor de π = 3.14159
+           
             int a, b, c, d, df;
             
             a = int.Parse(Console.ReadLine());
